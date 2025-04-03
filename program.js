c6ad12538c0dc5870ebe53c3e5544a79ab1041e7
Santosh Singh Pawar <textingsantosh@gmail.com>
2025-04-03 11:22:08 +0530

    program made

---
+++ program.js
@@ -0,0 +1,32 @@
+const library = {
+    
+    books: [{ title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 }],
+    addBook(book) {
+    if (!book.title && !book.author ) {
+    console.log("Book information is incomplete.");
+    return;
+    }
+    book?.title ?? "Title is missing"
+    book?.author ?? "Author is missing"
+    book?.year ?? "Year is missing"
+    this.books.push(book);
+    console.log("Book Added")
+
+},
+
+findBookByTitle(title) {
+return this.books.find(book => book.title === title);
+},
+
+removeBook(title) {
+const index = this.books.findIndex(book => book.title === title);
+if (index !== -1) {
+this.books.splice(index, 1);
+} else {
+console.log("Book not found.");
+} } };
+
+library.addBook({ author: "George Orwell", year: 1949 });
+
+
+console.log(library.books.length);
